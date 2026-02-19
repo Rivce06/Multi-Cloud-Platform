d0e811c17f16cfd1d9b0806ac5428b260a3ccf78
Rivce06 <gvenegas7978@gmail.com>
2026-02-19 17:07:51 +0000

    test

--- gcp/dev/apps-bootstrap/argocd/main.tf
+++ gcp/dev/apps-bootstrap/argocd/main.tf
@@ -1,15 +1,21 @@
 terraform {
+  required_version = ">= 1.10.0" 
+
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.17.0"
+      version = "~> 3.1.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0.1"
     }
   }
 }
 
 resource "helm_release" "argocd" {
   name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
+  repository       = "https://argoproj.github.io"
   chart            = "argo-cd"
   namespace        = "argocd"
   create_namespace = true
@@ -18,5 +24,42 @@ resource "helm_release" "argocd" {
   set {
     name  = "server.service.type"
     value = "ClusterIP"
+  }
+
+  set {
+    name  = "configs.cm.kubernetes.resource.patch.serverSideApply"
+    value = "true"
+  }
+}
+
+resource "kubernetes_manifest" "root_app" {
+  depends_on = [helm_release.argocd]
+
+  manifest = {
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind"       = "Application"
+    "metadata" = {
+      "name"      = "root-app"
+      "namespace" = "argocd"
+    }
+    "spec" = {
+      "project" = "default"
+      "source" = {
+        "repoURL"        = "https://github.com"
+        "targetRevision" = "main"
+        "path"           = "bootstrap"
+      }
+      "destination" = {
+        "server"    = "https://kubernetes.default.svc"
+        "namespace" = "argocd"
+      }
+      "syncPolicy" = {
+        "automated" = {
+          "prune"    = true
+          "selfHeal" = true
+        }
+        "syncOptions" = ["CreateNamespace=true"]
+      }
+    }
   }
 }
